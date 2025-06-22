7b70d2ddbbf36054858d93ddcfc5f9a6af09164d
Rizky Yoga <rizkyyogasalasa@gmail.com>
2025-06-22 13:23:47 +0700

    Fix: Set Node.js version to 20 and disable production source maps

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  productionBrowserSourceMaps: false, // Disable source maps in production
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals.push('ws');
